51a87089fddfe135c97d5700440db79524b3567a
Santi Tabbach <tabbach97@gmail.com>
2024-06-20 12:02:05 -0300

    Change colors scheme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,34 +1,34 @@
-const tintColorLight = "#0d1e53";
-const tintColorDark = "#e9ecfa";
+const tintColorLight = "#407A7E";
+const tintColorDark = "#B6E6CD";
 
 export const Colors = {
   light: {
-    background: "#e9ecfa",
-    secondary: "#FFFF",
+    background: "#FAFAFA",
+    secondary: "#E5E5E5",
     tint: tintColorLight,
     icon: "#31363F",
     tabIconDefault: "#31363F",
     tabIconSelected: tintColorLight,
     text: {
-      default: "#4a4670",
-      defaultSemiBold: "#9aa1c1",
+      default: "#31363F",
+      defaultSemiBold: "#989AA0",
       title: tintColorLight,
-      subtitle: "#4a4670",
+      subtitle: "#31363F",
       link: tintColorLight,
     },
   },
   dark: {
-    background: "#0d1e53",
-    secondary: "#1f3165",
+    background: "#171717",
+    secondary: "#262626",
     tint: tintColorDark,
     icon: "#EEEEEE",
     tabIconDefault: "#EEEEEE",
     tabIconSelected: tintColorDark,
     text: {
-      default: "#c0d1dd",
-      defaultSemiBold: "#536497",
+      default: "#FFFF",
+      defaultSemiBold: "#737373",
       title: tintColorDark,
-      subtitle: "#c0d1dd",
+      subtitle: "#FFFF",
       link: tintColorDark,
     },
   },
